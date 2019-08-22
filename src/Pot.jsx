2579f5c31c718faca7da1a64bf76b3ed98cc6c5b
noel <noelsoong@live.com>
2019-08-22 15:57:43 +1200

    ignore exhaustive deps

--- src/Pot.jsx
+++ src/Pot.jsx
@@ -14,6 +14,7 @@ const Pot = () => {
   const displayEmpty = () => <p>Sorry no more cards</p>
   useEffect(() => {
     dispatch(getCardsSuccess())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
