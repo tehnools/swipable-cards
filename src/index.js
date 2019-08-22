1bdf1c0318b81fdfa97709a899d08dc51805f344
noel <noelsoong@live.com>
2019-08-22 21:17:42 +1200

    Restore example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux'
+import './styles.css'
+import rootReducer from './reducers/index'
+import Pot from './Pot'
 
 module.exports = {
   SwipeCard: require('./SwipeCard'),
   SwipeCards: require('./SwipeCards')
 }
+
+const store = createStore(
+  rootReducer,
+  {},
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  ))
+
+function App () {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Pot />
+      </div>
+    </Provider>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
