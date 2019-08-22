b80c22d272cd23993534badc10981fc63cd7208e
noel <noelsoong@live.com>
2019-08-22 14:11:51 +1200

    window.devToolsExtention :arrow_right:  window.__REDUX_DEVTOOLS_EXTENSION__

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const store = createStore(
   {},
   compose(
     applyMiddleware(thunk),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
   ))
 
 function App() {
